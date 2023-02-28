6badd4f317629a9b124367d61dfc94c6dd7bcb1c
Mohammed Mubashir <mohammedmubashir204@gmail.com>
2023-02-28 20:52:35 +0530

    - Make models folder - Make models for Post and network data

---
+++ LinkedIn Clone/Models/NetworkModel.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkModel.swift
+//  LinkedIn Clone
+//
+//  Created by Mohammed Mubashir on 28/02/23.
+//
+
+import Foundation
+
+struct NetworkModel {
+    var id: Int
+    var image:String
+    var name :String
+    var position : String
+    var mutual: String
+    
+}
---
+++ LinkedIn Clone/Models/PostModel.swift
@@ -0,0 +1,17 @@
+//
+//  PostModel.swift
+//  LinkedIn Clone
+//
+//  Created by Mohammed Mubashir on 28/02/23.
+//
+
+import Foundation
+
+struct postData{
+    var id:Int
+    var image:String
+    var title:String
+    var followers:Int
+    var profileImage:String
+    
+}
